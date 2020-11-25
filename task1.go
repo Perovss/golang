5de11be5cbf6f57b01a98cec44e62acabcbfbd6d
Перов Сергей <perov.ss@gmail.com>
2020-11-25 17:24:08 +0300

    task 1-3

---
+++ task1.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("Введите количество метров: ")
+	var input float32
+	fmt.Scanf("%f", &input)
+
+	output := input / 0.3048
+
+	fmt.Println("Количество футов: "output)
+}
