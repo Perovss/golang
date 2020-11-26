459f659789e6e9730de822e6e4b2a13b7828aece
Перов Сергей <perov.ss@gmail.com>
2020-11-26 14:22:17 +0300

    task 1-2

--- task2.go
+++ task2.go
@@ -2,15 +2,16 @@ package main
 
 import "fmt"
 
-var y int
-
 func main() {
 	x := []int{48, 96, 86, 68, 57, 82, 63, 70, 37, 34, 83, 27, 19, 97, 9, 17}
 	count := len(x)
-	for count > 1 {
-		count = count - 1
-		y := x[count]
-		fmt.Println(y)
+	element := x[count-1]
+	for count > 0 {
+		count -= 1
+		minimum := x[count]
+		if element > minimum {
+			element = minimum
+		}
 	}
-
+	fmt.Println("Минимальное число: ", element)
 }
