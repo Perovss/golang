aef2d785405b0f326d941137479778972a89b2d9
Перов Сергей <perov.ss@gmail.com>
2020-11-26 13:52:02 +0300

    task 1-2

--- task2.go
+++ task2.go
@@ -2,8 +2,15 @@ package main
 
 import "fmt"
 
+var y int
+
 func main() {
 	x := []int{48, 96, 86, 68, 57, 82, 63, 70, 37, 34, 83, 27, 19, 97, 9, 17}
 	count := len(x)
-	fmt.Println(count)
+	for count > 1 {
+		count = count - 1
+		y := x[count]
+		fmt.Println(y)
+	}
+
 }
